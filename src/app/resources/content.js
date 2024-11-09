59416ea0a8386893b8ecb88f422cca8ca2442f0e
1mshy <imshi1@protonmail.com>
2024-11-09 14:29:51 -0500

    test

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -83,7 +83,7 @@ const about = {
                 timeframe: '2022 - Present',
                 role: 'Senior Design Engineer',
                 achievements: [
-                    <>Redesigned the UI/UX for the FLY platform, resulting in a 20% increase in user engagement and 30% faster load times.</>,
+                    <>the UI/UX for the FLY platform, resulting in a 20% increase in user engagement and 30% faster load times.</>,
                     <>Spearheaded the integration of AI tools into design workflows, enabling designers to iterate 50% faster.</>
                 ],
                 images: [ // optional: leave the array empty if you don't want to display images
